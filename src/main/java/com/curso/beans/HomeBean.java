f4ac21bb24b7ffaf8c7ea622770123a9a5df8481
guilherme.storti <Gvale@2017>
2017-05-11 20:02:47 -0300

    Upload 2

--- src/main/java/com/curso/beans/HomeBean.java
+++ src/main/java/com/curso/beans/HomeBean.java
@@ -1,6 +1,10 @@
 package com.curso.beans;
 
+import com.curso.entity.Arquivo;
+import com.curso.entity.Turma;
+import com.curso.entity.Usuario;
 import com.curso.utils.ArquivoUtil;
+import com.curso.utils.JpaUtil;
 import org.primefaces.model.UploadedFile;
 
 import javax.annotation.PostConstruct;
@@ -8,6 +12,7 @@ import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
+import javax.persistence.EntityManager;
 import java.io.File;
 import java.io.IOException;
 import java.io.Serializable;
@@ -33,14 +38,39 @@ public class HomeBean implements Serializable{
         try {
             File arquivo = ArquivoUtil.escrever(uploadedFile.getFileName(), uploadedFile.getContents());
 
-            arquivos.add(arquivo);
+            persistir();
+            //arquivos.add(arquivo);
 
             FacesContext.getCurrentInstance().addMessage(null,
                     new FacesMessage("Upload completo", "O arquivo " + arquivo.getName() + " foi salvo!"));
+            limpar();
         } catch (IOException e) {
             FacesContext.getCurrentInstance().addMessage(null,
                     new FacesMessage(FacesMessage.SEVERITY_WARN, "Erro", e.getMessage()));
         }
+    }
+
+    private void persistir() {
+        EntityManager manager = JpaUtil.getManager();
+        manager.getTransaction().begin();
+        Arquivo arquivo = new Arquivo();
+        arquivo.setDados(uploadedFile.getContents());
+        arquivo.setDescricao(uploadedFile.getFileName());
+
+        List<Turma> turmas = manager.createQuery("from Turma", Turma.class).getResultList();
+
+        List<Usuario> usuarios = manager.createQuery("from Usuario", Usuario.class).getResultList();
+
+        arquivo.setIdTurma(turmas.get(0));
+        arquivo.setIdUsuarioUpload(usuarios.get(0));
+        manager.merge(arquivo);
+        manager.getTransaction().commit();
+        manager.close();
+    }
+
+    private void limpar() {
+        this.arquivos.clear();
+        this.uploadedFile = null;
     }
 
     public List<File> getArquivos() {
